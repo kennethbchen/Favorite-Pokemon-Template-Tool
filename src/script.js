4fe54d8aa929745d88fd04ab3327f2ec3c07e574
kennethbchen <kennethbchen@gmail.com>
2021-11-16 23:43:59 -0500

    Moved index to root and reformatted code

--- src/script.js
+++ src/script.js
@@ -5,8 +5,11 @@ const borderSize = 1;
 const gridWidth = 23;
 const gridHeight = 10;
 
+const testImageLocation = "/ignore/test2.png";
 const templateLocation = "/resources/template modified.png";
+
 const tableDiv = document.getElementById("tableDiv");
+const mainImageTag = document.getElementById("mainImage");
 
 var table;
 
@@ -18,31 +21,6 @@ const colHeaders =
 const rowHeaders =
 ["", "I", "II", "III", "IV", "V", "VI", "VII", "VII", "Favorite"]
 
-// Starts top left at (0,0)
-// Gives top left pixel coordinate of box [x,y]
-function coordToPixel(x, y) {
-    // Total length from borders
-    var borderX = x * borderSize + 1;
-    var borderY = y * borderSize + 1;
-
-    // Total Length from boxes
-    var boxX = x * boxLength;
-    var boxY = y * boxLength;
-
-    var xCoord = borderX + boxX;
-    var yCoord = borderY + boxY;
-
-    if ( x < 0 || x >= gridWidth) {
-        xCoord = 1;
-    }
-
-    if (y < 0 || y >= gridHeight) {
-        yCoord = 1;
-    }
-    return [xCoord, yCoord];
-}
-
-
 // Initialize tableDiv
 tableOutput = "<table id=\"mainTable\">\n";
 
@@ -52,6 +30,7 @@ for(row = 0; row < gridHeight; row++) {
     for(col = 0; col < gridWidth; col++) {
 
         if (row + col == 0) {
+            // Handle Top left cell
             tableOutput += "<th></th>";
         } else if (row == 0) {
             // Create Vertical Headings
@@ -72,6 +51,31 @@ tableOutput += "</table>";
 tableDiv.innerHTML = tableOutput;
 
 table = document.getElementById("mainTable");
+
+
+// Starts top left at (0,0)
+// Gives top left pixel coordinate of box [x,y]
+function coordToPixel(row, col) {
+    // Total length from borders
+    var borderX = col * borderSize + 1;
+    var borderY = row * borderSize + 1;
+
+    // Total Length from boxes
+    var boxX = col * boxLength;
+    var boxY = row * boxLength;
+
+    var xCoord = borderX + boxX;
+    var yCoord = borderY + boxY;
+
+    if ( col < 0 || col >= gridWidth) {
+        xCoord = 1;
+    }
+
+    if (row < 0 || row >= gridHeight) {
+        yCoord = 1;
+    }
+    return [xCoord, yCoord];
+}
 
 
 // Returns a 2 dimensional list of all cell values in the input table ([row][column])
@@ -95,4 +99,29 @@ function getCellValues() {
     }
 
     return output;
+}
+
+// Takes the input values from the data and renders the final image
+function renderImage() {
+    var tableData = getCellValues();
+
+    var imageData = [];
+
+    // Add the base template
+    imageData.push({src: templateLocation, x: 0, y:0});
+
+    // Add images based on the table data
+    for(var row = 0; row < tableData.length; row++) {
+        for (var col = 0; col < tableData[row].length; col++) {
+
+            if (tableData[row][col] !== "") {
+                coordinates = coordToPixel(row + 1, col + 1);
+
+                // Todo, translation between table data and image
+                imageData.push({src: testImageLocation, x: coordinates[0], y: coordinates[1]});
+            }
+        }
+    }
+
+    mergeImages(imageData).then(b64 => mainImageTag.src = b64);
 }
